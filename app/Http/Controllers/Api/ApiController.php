4b996d03cebb1e8363aa686fb07f29e233d296dd
Vũ Tuân <75652084+vuhuyentuan@users.noreply.github.com>
2021-10-10 18:51:13 +0700

    update

--- app/Http/Controllers/Api/ApiController.php
+++ app/Http/Controllers/Api/ApiController.php
@@ -4,20 +4,26 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use App\Models\ServiceBill;
+use Illuminate\Support\Facades\Cache;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
 
 class ApiController extends Controller
 {
     public function getOrder()
     {
-        $i = 1;
-        $order = ServiceBill::count();
-        if($order > 0){
-            $i++;
-            return response()->json([
-                'success' => true,
-                'message' => 'There are '.$i. 'order'
-            ]);
+        $order = ServiceBill::where('status', 0)->first();
+        if($order){
+            $lock = Cache::lock($order->id, 10);
+            if ($lock->get()) {
+                return response()->json([
+                    'success' => true,
+                    'message' => 'Have an order'
+                ]);
+                $lock->release();
+            }else{
+                return Http::get(url("/api/order"));
+            }
         }else{
             return response()->json([
                 'success' => false,
